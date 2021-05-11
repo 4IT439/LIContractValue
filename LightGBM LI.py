cd27eff79633edcedd77f85d1ddaba419c9a387e
Marek Rotrekl <github@rotrekl.name>
2021-05-11 23:30:09 +0200

    Metrics update

--- LightGBM LI.py
+++ LightGBM LI.py
@@ -65,8 +65,10 @@ X_train_valid, X_test_valid, y_train_valid, y_test_valid = train_test_split(
     random_state = SEED
     )
 
-#setup params grid
+X_train_valid, y_train_valid, X_test_valid, y_test_valid = X_train, y_train, X_test, y_test
 
+
+#setup params grid
 import random
 
 GRID_SIZE = 20
@@ -81,7 +83,7 @@ grid = pd.DataFrame({
     })
 
 
-def fit_regressor(X_train, y_train, params):
+def fit_regressor(X_train, y_train, X_test, y_test, params):
     # Instantiate a lgb.LGBMRegressor
     lgbm0 = lgb.LGBMRegressor(seed=SEED,
     #n_estimators=params['n_estimators'],
@@ -99,60 +101,28 @@ def fit_regressor(X_train, y_train, params):
     return {'MAPE': MAPE_test0}
 
 
+import time
+
 # fit regressor and compute MAPE for each param vector
+tic = time.perf_counter() #begin timing
 MAPE_list = np.empty(grid.shape[0])
 for i in range(grid.shape[0]):
     MAPE_list[i] = fit_regressor(
-        X_train_valid, y_train_valid, grid.iloc[i]
+        X_train_valid, y_train_valid,
+        X_test_valid, y_test_valid,
+         grid.iloc[i]
         )['MAPE']
-
-#add MAPE to grid  
-grid['MAPE'] = MAPE_list
-
-
-########################
-####Grid optimization###
-########################
-
-#setup params grid
-param_grid = {'learning_rate': [0.01,0.2,0.5], #alias eta, Step size shrinkage used in update to prevents overfitting.  
-    'n_estimators': [20, 50, 100],
-    'max_depth': [3, 5, 10],
-    'num_leaves': [16, 64, 128], 
-    'min_data_in_leaf' : [16, 64, 128]
-    }
-
-
-
-
-from sklearn. model_selection import GridSearchCV
-import time
-
-#instantiate LGBMRegressor 
-lgbm = lgb.LGBMRegressor(seed=SEED)
-grid_mse = GridSearchCV(estimator=lgbm,
-                        param_grid=param_grid,
-                        scoring=mape_scorer, 
-                        cv=3, 
-                        verbose=1, 
-                        n_jobs=1)
-#fit  GridSearchCV 
-tic = time.perf_counter() #begin timing
-grid_mse.fit(X_train, y_train)
 time_fit_cv = time.perf_counter() - tic #save timer
+grid['MAPE'] = MAPE_list #add MAPE to grid  
 
-print("Best parameters found: ",grid_mse.best_params_) #best_params_
-print("Lowest MAPE found: ", np.abs(grid_mse.best_score_)) #best_score_
 
-#extract the estimator best_estimator_ 
-lgbm_ins = grid_mse.best_estimator_ #best_estimator_
+best_fit_no = np.argmin(grid['MAPE'])
 
-# Predict the test set labels 'y_pred'
-y_pred = lgbm_ins.predict(X_test)
+low_MAPE = grid.iloc[best_fit_no]['MAPE']
+best_params = grid.iloc[best_fit_no].drop('MAPE').to_dict()
+train_size = X_train_valid.shape[0]
 
-# Evaluate the test set R2score
-mape_test = mean_absolute_percentage_error(y_test, y_pred)
-print(mape_test)
+print(low_MAPE)
 
 
 #send performance metrics to a google sheet,
@@ -162,58 +132,19 @@ import requests, datetime, json
 requests.post(
     "https://sheet.best/api/sheets/6a3a81b3-be98-409b-9d40-8de4e0b3ee26",
     json={
-        'Name': '____',
-        'TEST': 'GRID',
+       'Name': '____TEST',
+        'TEST': 'VECTOR',
         'RMSE': 'N/A',
         'DATETIME': datetime.datetime.now().isoformat(),
         'SEED': SEED,
         'RATIO': 'N/A',
-        'PARAM_GRID': json.dumps(param_grid, indent=0),
-        'BEST_PARAMS':json.dumps(grid_mse.best_params_, indent=0),
+        'PARAM_GRID': 'N/A',
+        'BEST_PARAMS':json.dumps(best_params, indent=0),
         'TIME_FIT': time_fit_cv,
         'LOW_RMSE': 'N/A',
-        'MAPESCORE': mean_absolute_percentage_error(y_test, y_pred)
-    },
+        'MAPESCORE': low_MAPE,
+        'N_ROWS_TRAIN': train_size
+    }
 )
 
-
-###################################
-####Grid randomized optimization###
-###################################
-
-from sklearn.model_selection import RandomizedSearchCV
-
-#setup params grid
-param_grid = {'learning_rate': [0.01,0.1,0.5], #alias eta, Step size shrinkage used in update to prevents overfitting.  
-    'n_estimators': [20, 50, 100],
-    'subsample': [0.5, 0.8, 1], #Subsample ratio of the training instances
-    'max_depth': [3, 5, 10],
-    'colsample_bytree': [0.5, 1] #colsample_bytree is the subsample ratio of columns when constructing each tree. Subsampling occurs once for every tree constructed.
-    }
-
-#instantiate LGBMRegressor 
-lgbm = lgb.LGBMRegressor(seed=SEED)
-grid_mse = RandomizedSearchCV(estimator=lgbm,
-                        param_distributions=param_grid,
-                        scoring='neg_mean_squared_error', 
-                        cv=3, 
-                        verbose=1, 
-                        n_jobs=1)
-#fit  GridSearchCV 
-tic = time.perf_counter() #begin timing
-grid_mse.fit(X_train, y_train)
-time_fit_cv = time.perf_counter() - tic #save timer
-
-print("Best parameters found: ",grid_mse.best_params_) #best_params_
-print("Lowest RMSE found: ", np.sqrt(np.abs(grid_mse.best_score_))) #best_score_
-
-#extract the estimator best_estimator_ 
-lgbm_ins = grid_mse.best_estimator_ #best_estimator_
-
-# Predict the test set labels 'y_pred'
-y_pred = lgbm_ins.predict(X_test)
-
-# Evaluate the test set RMSE
-rmse_test = mean_squared_error(y_test, y_pred, squared=False)
-print(rmse_test)
 
